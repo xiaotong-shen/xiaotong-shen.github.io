7c9bdf5a8a2804ad13dbdaa3a92c466cb6fa0d69
xiaotong-shen <shenxiaotong13@gmail.com>
2025-03-10 22:19:26 -0400

    branch first commit

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  base: './',
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        format: 'es',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    loader: 'jsx'
-  }
 })
